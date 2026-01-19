b44ec6b6c4cd2e8c3646e433b8bb7943c29428fe
gunashekar751-svg <gunashekar751@gmail.com>
2026-01-19 20:57:25 +0530

    Delete file.py

--- file.py
+++
@@ -1,8 +0,0 @@
-try:
-    with open("sample.txt", "rt") as file:
-        print("Reading file content:")
-        for line in file:
-            print(line.strip())
-except FileNotFoundError:
-    print(" The file 'sample.txt' was not found.")
-
