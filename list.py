7263c0d9b0b0f797afce05da2f47d6e72dc6bb8b
gunashekar751-svg <gunashekar751@gmail.com>
2026-01-19 20:57:47 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,6 @@
+l1 = [1,2,3,4,5,6,7,8,9,10]
+print(f"Orignal list: {l1}")
+five_elements = l1[:5]
+print(f"Extracted first five elements: {five_elements}")
+five_elements.reverse()
+print(f"Reversed extracted elements: {five_elements}")
